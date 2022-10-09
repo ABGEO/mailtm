23556af5619d03f3fb6aef4b4e6723096145e300
Temuri Takalandze <takalandzet@gmail.com>
2022-10-10 00:21:33 +0400

    test: cover RandomString with tests

---
+++ pkg/util/random_test.go
@@ -0,0 +1,23 @@
+package util
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+)
+
+type RandomSuite struct {
+	suite.Suite
+}
+
+func TestRandomSuiteSuite(t *testing.T) {
+	suite.Run(t, new(RandomSuite))
+}
+
+func (suite *HTTPErrorSuite) TestRandomString() {
+	random := RandomString(16)
+
+	assert.Equal(suite.T(), 16, len(random))
+	assert.Regexp(suite.T(), `^[a-zA-Z0-9]*$`, random)
+}
